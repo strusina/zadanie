1c0749b12d752058c8d0fed49cc46d7daeb4e3e7
strusina <radekstrus09@gmail.com>
2026-03-26 10:13:09 +0100

    piotr11

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void(String[] args){
 		Samochud car1 = new Samochud("Audi", "A3", 2000);
-		Samochud car2 = new Samochud("Opel", "Astra", 2001);
+		Samochud car2 = new Samochud("Opel", "Astra", 200)dyuksagdyagdydbsadasdg;
 	}
 }
